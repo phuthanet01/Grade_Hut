4827c8f1766a9f166fe19575120ae22a23881357
phuthanet01 <phuthanet.son@sbac.ac.th>
2023-08-17 13:49:03 +0700

    Add project files.

---
+++ Grade_Hut/คำนวณเกรด.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Grade_Hut
+{
+    partial class คำนวณเกรด
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(185, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 40);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "คำนวณ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtScore
+            // 
+            this.txtScore.Location = new System.Drawing.Point(151, 94);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(218, 22);
+            this.txtScore.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(103, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(330, 32);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "ใส่คะแนน เพื่อ คำนวณเกรด";
+            // 
+            // คำนวณเกรด
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 223);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.label1);
+            this.Name = "คำนวณเกรด";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "โปรแกรมคำนวณเกรด";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Grade_Hut/คำนวณเกรด.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grade_Hut
+{
+    public partial class คำนวณเกรด : Form
+    {
+        public คำนวณเกรด()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // อ่านคะแนนจาก TextBox
+            if (int.TryParse(txtScore.Text, out int score))
+            {
+                // คำนวณเกรด
+                string grade;
+                if (score >100)
+                    grade = "S+ เลยไหมคะแนนเกิน 100 แล้ว";
+                else if (score >= 80 && score <= 100)
+                    grade = "A";
+                else if (score >= 75 && score <= 79)
+                    grade = "B+";
+                else if (score >= 70 && score <= 74)
+                    grade = "B";
+                else if (score >= 65 && score <= 69)
+                    grade = "C+";
+                else if (score >= 60 && score <= 64)
+                    grade = "D";
+                else if (score >= 55 && score <= 59)
+                    grade = "D";
+                else if (score >= 50 && score <= 54)
+                    grade = "D";
+                else
+                    grade = "F";
+
+                // แสดงผลลัพธ์ใน MessageBox
+                MessageBox.Show($"คะแนน {score} คะแนน ได้เกรด {grade}", "ผลลัพธ์", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("โปรดกรอกข้อมูลที่ถูกต้องให้ครบถ้วน", "ข้อผิดพลาด", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
